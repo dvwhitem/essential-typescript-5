c381477cb3f03727f8c9660a3c7b7b91157dd107
Vitaliy <dvwhitem@gmail.com>
2024-08-13 18:36:43 +0300

    polish

---
+++ chapter-03/primer/index.js
@@ -0,0 +1,12 @@
+let hatPrice = 100;
+console.log(`Hat price ${hatPrice}`);
+let bootPrice = '100';
+console.log(`Boot price ${bootPrice}`);
+
+if(hatPrice == bootPrice) {
+    console.log('Same price')
+} else {
+    console.log('Difference price')
+}
+let totalPrice = hatPrice + bootPrice
+console.log(`Total price: ${totalPrice}`)
