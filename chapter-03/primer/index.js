22ea6c763d0cf3113ceeac8e95c925e54c246888
Vitaliy <dvwhitem@gmail.com>
2024-08-14 01:39:51 +0300

    notice: type of null is object

--- chapter-03/primer/index.js
+++ chapter-03/primer/index.js
@@ -4,9 +4,16 @@ let bootPrice = '100';
 console.log(`Boot price ${bootPrice}`);
 
 if(hatPrice == bootPrice) {
-    console.log('Same price')
+    console.log('Same price');
 } else {
-    console.log('Difference price')
+    console.log('Difference price');
 }
-let totalPrice = hatPrice + bootPrice
-console.log(`Total price: ${totalPrice}`)
+let totalPrice = hatPrice + bootPrice;
+console.log(`Total price: ${totalPrice}`);
+
+let myVar = 'Roque';
+console.log(`Type of myVar is ${typeof myVar}`);
+myVar = 100;
+console.log(`Type of myVar is ${typeof myVar}`);
+
+console.log(`TYpe of null is ${typeof null}`)
