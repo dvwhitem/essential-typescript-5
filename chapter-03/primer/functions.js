4e8f98b8768d1131e1dfaf930cdb10240519b249
Vitaliy <dvwhitem@gmail.com>
2024-08-15 02:29:26 +0300

    example functions

---
+++ chapter-03/primer/functions.js
@@ -0,0 +1,32 @@
+// function sumPrices(first, second, third = 0) {
+//     return first + second + third
+// }
+
+function sumPrices(...numbers) {
+    return  numbers.reduce(function (total, val) {
+        return total + (Number.isNaN(Number(val)) ? 0 : Number(val))
+    }, 0)
+}
+
+let hatPrice = 100
+console.log(`Hat price: (${typeof hatPrice}) ${hatPrice}`)
+let bootsPrice = '100'
+console.log(`Boots price: (${typeof bootsPrice}) ${bootsPrice}`)
+
+let totalPrice = sumPrices(hatPrice, bootsPrice)
+console.log(`Total Price ${totalPrice} (${typeof totalPrice})`)
+
+totalPrice = sumPrices(100, 200, 300)
+console.log(`Total Price ${totalPrice} (${typeof totalPrice})`)
+
+totalPrice = sumPrices(100, 200)
+console.log(`Total Price ${totalPrice} (${typeof totalPrice})`)
+
+totalPrice = sumPrices(100, 200, 400, 'test', null, undefined, false)
+console.log(`Total Price ${totalPrice} (${typeof totalPrice})`)
+
+console.log(`Number (null): ${Number(null)}`)
+console.log(`Number (false): ${Number(false)}`)
+console.log(`Number ('test'): ${Number('test')}`)
+console.log(`Number (undefined): ${Number(undefined)}`)
+console.log(`Number (0) ? ${Number(0)}`)
--- chapter-03/primer/index.js
+++ chapter-03/primer/index.js
@@ -1,23 +1,29 @@
-let hatPrice = 100;
-console.log(`Hat price ${hatPrice}`);
-let bootPrice = '100';
-console.log(`Boot price ${bootPrice}`);
+let hatPrice = 100
+console.log(`Hat price ${hatPrice}`)
+let bootPrice = '100'
+console.log(`Boot price ${bootPrice}`)
 
 if (hatPrice === bootPrice) {
-    console.log('Same price');
+    console.log('Same price')
 } else {
-    console.log('Difference price');
+    console.log('Difference price')
 }
-let totalPrice = Number(hatPrice) + Number(bootPrice);
-console.log(`Total price: ${totalPrice}`);
+let totalPrice = Number(hatPrice) + Number(bootPrice)
+console.log(`Total price: ${totalPrice}`)
 
-let myVar = 'Roque';
-console.log(`Type of myVar is ${typeof myVar}`);
-myVar = 100;
-console.log(`Type of myVar is ${typeof myVar}`);
+let myVar = 'Roque'
+console.log(`Type of myVar is ${typeof myVar}`)
+myVar = 100
+console.log(`Type of myVar is ${typeof myVar}`)
 
-console.log(`TYpe of null is ${typeof null}`);
+console.log(`TYpe of null is ${typeof null}`)
 
 let firstCity = 'Manchester'
-let secondCity = firstCity || 'London';
+let secondCity = firstCity || 'London'
 console.log(`City: ${secondCity}`)
+
+let textRate
+console.log(`Text rate: ${textRate ?? 10}%`)
+textRate = 0
+console.log(`Text rate: ${textRate ?? 10}%`)
+
