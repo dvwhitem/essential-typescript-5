d7d04dc5d90a97449deda3113730b785ecd7d4fc
Vitaliy <dvwhitem@gmail.com>
2024-08-30 00:32:37 +0300

    start prototype

--- chapter-03/primer-p2/index.js
+++ chapter-03/primer-p2/index.js
@@ -1,10 +1,35 @@
 let hat = {
-    name: "Hat",
+    name: 'Hat',
     price: 100,
-    getPriceincTax() {
+    getPriceIncTax() {
         return Number(this.price) * 1.2;
     }
 }
+let boots = {
+    name: 'Boots',
+    price: 100,
+    getPriceIncTax() {
+        return Number(this.price) * 1.2
+    }
+}
 
-console.log(`Hat: ${hat.price}, ${hat.getPriceincTax()}`)
-console.log(`To String: ${hat.toString()}`)
+let hatPrototype = Object.getPrototypeOf(hat)
+console.log(`Hat Prototype: ${hatPrototype}`);
+
+let bootsPrototype = Object.getPrototypeOf(boots)
+console.log(`Boots Prototype: ${bootsPrototype}`)
+
+console.log(`Common prototype: ${ hatPrototype === bootsPrototype}`);
+
+console.log(`Hat: ${hat.price}, ${hat.getPriceIncTax()}`)
+console.log(`Prototype ${hat.toString()}`)
+console.log(`Type of hat: ${typeof hat}`)
+
+console.log('------------------------')
+hatPrototype.toString = function () {
+    return `toString: Name: ${this.name} Price: ${this.price}`
+}
+
+console.log(`Prototype ${hat.toString()}`)
+console.log(hat.toString())
+console.log(boots.toString())
